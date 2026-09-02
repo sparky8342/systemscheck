f6b57660efc6aaecbb6c05938b75eb7e42b36a3d
David Bell[C <david.bell@gmail.com>
2026-09-02 19:26:39 +0100

    mission 6 part 2

--- missions/mission6.pl
+++ missions/mission6.pl
@@ -14,14 +14,17 @@ for my $line (@data) {
 	push @asteroids, { x => $x, y => $y, vx => $vx, vy => $vy };
 }
 
-my $collisions = 0;
+my %collisions;
 for my $y (1..10000) {
 	for my $asteroid (@asteroids) {
 		$asteroid->{x} += $asteroid->{vx};
 		$asteroid->{y} += $asteroid->{vy};
-		if ($asteroid->{x} == 0 && $asteroid->{y} == $y) {
-			$collisions++;
+		for my $x (-10..10) {
+			if ($asteroid->{x} == $x && $asteroid->{y} == $y) {
+				$collisions{$x}++;
+			}
 		}
 	}
 }
-print "$collisions\n";
+
+printf("%d\n%d\n", $collisions{0}, (sort { $a <=> $b } values %collisions)[0]);
