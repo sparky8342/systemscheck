f76b7b14546321bf57b1cafa50e4399f27d09914
David Bell[C <david.bell@gmail.com>
2026-09-02 19:40:29 +0100

    mission 7 part 2

--- missions/mission7.pl
+++ missions/mission7.pl
@@ -9,12 +9,18 @@ close $fh;
 chomp($_) foreach @data;
 
 my $sum = 0;
+my %loads;
 foreach my $line (@data) {
-	my (undef, $in, $out) = split/ /, $line;
+	my ($id, $in, $out) = split/ /, $line;
 	my $efficiency = int(100 * $out / $in);
 	if ($efficiency < 97) {
 		$sum += $efficiency;
+		($id) = $id =~ /^\d+\|(.*)$/;
+		while ($id =~ /(\w{3})/g) {
+			$loads{$1} += $efficiency;
+		}
 	}
+
 }
 
-printf("%d\n", $sum);
+printf("%d\n%d\n", $sum, (sort { $b <=> $a } values %loads)[0]);
